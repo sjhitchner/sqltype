7d7c2aea1980502c6b2f623334624721a3870b17
Stephen Hitchner <stephen@kiip.me>
2016-10-25 18:08:28 -0700

    updating examples, improving template

---
+++ cmd/sqltype/sqltype_test.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestSqlTypeString(t *testing.T) {
+	scanner := Scanner{
+		Package:   "test",
+		Primative: "string",
+		Type:      "TestType",
+	}
+
+	buf := &bytes.Buffer{}
+
+	if err := SqlType(buf, scanner); err != nil {
+		t.Fatal(err)
+	}
+
+	if buf.Len() == 0 {
+		t.Fatalf("Nothing generated")
+	}
+}
+
+func TestSqlTypeInt(t *testing.T) {
+	scanner := Scanner{
+		Package:   "test",
+		Primative: "int",
+		Type:      "TestType",
+	}
+
+	buf := &bytes.Buffer{}
+
+	if err := SqlType(buf, scanner); err != nil {
+		t.Fatal(err)
+	}
+
+	if buf.Len() == 0 {
+		t.Fatalf("Nothing generated")
+	}
+}
+
+func TestSqlTypeBool(t *testing.T) {
+	scanner := Scanner{
+		Package:   "test",
+		Primative: "bool",
+		Type:      "TestType",
+	}
+
+	buf := &bytes.Buffer{}
+
+	if err := SqlType(buf, scanner); err != nil {
+		t.Fatal(err)
+	}
+
+	if buf.Len() == 0 {
+		t.Fatalf("Nothing generated")
+	}
+}
+
+func TestSqlTypePythonDict(t *testing.T) {
+	scanner := Scanner{
+		Package:   "test",
+		Primative: "pythondict",
+		Type:      "TestType",
+	}
+
+	buf := &bytes.Buffer{}
+
+	if err := SqlType(buf, scanner); err != nil {
+		t.Fatal(err)
+	}
+
+	if buf.Len() == 0 {
+		t.Fatalf("Nothing generated")
+	}
+}
--- examples/example.go
+++ examples/example.go
@@ -14,45 +14,48 @@ import (
 
 const CreateSQL = `
 CREATE TABLE IF NOT EXISTS temporary (
+    id character varying(36) NOT NULL,
+    field1 boolean,
     field2 timestamp without time zone,
     field3 bytea,
-    field1 boolean,
-    field6 bytea,
-    field7 text,
     field4 bigint,
     field5 double precision,
-    id character varying(36) NOT NULL
+    field6 bytea,
+    field7 text,
+	field8 text
 );
-
-ALTER TABLE ONLY temporary ADD CONSTRAINT temporary_pkey PRIMARY KEY (id);
 `
-const SelectSQL = "SELECT id, field1, field2, field3, field4, field5, field6, field7 FROM temporary WHERE id=$1"
+
+//const SelectSQL = "SELECT id, field1, field2, field3, field4, field5, field6, field7 FROM temporary WHERE id=$1"
+const SelectSQL = "SELECT id, field1, field3, field4, field5, field7, field8 FROM temporary WHERE id=$1"
 
 //go:generate sqltype -primative string -type Id
 type Id string
 
-//go   generate sqltype -primative string -type Bool
 type Bool bool
 
 //go   generate sqltype -primative string -type time.Time
 type Time time.Time
-type Dictionary string
+
+//go:generate sqltype -primative pythondict -type Dictionary
+type Dictionary map[string]interface{}
 type Int int
 type Float float32
 type List string
+
+//go:generate sqltype -primative string -type String
 type String string
 
 type Primatives struct {
 	Id     string    `db:"id"`
 	Field1 bool      `db:"field1"`
 	Field2 time.Time `db:"field2"`
-	//Field3 Dictionary `db:"field3"`
-	Field3 string  `db:"field3"`
-	Field4 int     `db:"field4"`
-	Field5 float32 `db:"field5"`
-	//Field6 List       `db:"field6"`
-	Field6 string `db:"field6"`
-	Field7 string `db:"field7"`
+	Field3 string    `db:"field3"`
+	Field4 int       `db:"field4"`
+	Field5 float32   `db:"field5"`
+	Field6 string    `db:"field6"`
+	Field7 string    `db:"field7"`
+	Field8 *string   `db:"field8"`
 }
 
 func (t Primatives) String() string {
@@ -65,30 +68,33 @@ func (t Primatives) String() string {
 	fmt.Fprintf(buf, "Field5 float  %f\n", t.Field5)
 	fmt.Fprintf(buf, "Field6 list   %s\n", t.Field6)
 	fmt.Fprintf(buf, "Field7 string %s\n", t.Field7)
+	fmt.Fprintf(buf, "Field8 string %s\n", t.Field8)
 	return buf.String()
 }
 
 type Customs struct {
-	Id Id `db:"id"`
-	//Field1 Bool       `db:"field1"`
+	Id     Id   `db:"id"`
+	Field1 Bool `db:"field1"`
 	//Field2 Time       `db:"field2"`
-	//Field3 Dictionary `db:"field3"`
-	//Field4 Int        `db:"field4"`
-	//Field5 Float      `db:"field5"`
+	Field3 Dictionary `db:"field3"`
+	Field4 Int        `db:"field4"`
+	Field5 Float      `db:"field5"`
 	//Field6 List       `db:"field6"`
-	//Field7 String     `db:"field7"`
+	Field7 String `db:"field7"`
+	Field8 String `db:"field8"`
 }
 
 func (t Customs) String() string {
 	buf := &bytes.Buffer{}
-	fmt.Fprintf(buf, "Id     string %s\n", t.Id)
-	//fmt.Fprintf(buf, "Field1 bool   %t\n", t.Field1)
-	//fmt.Fprintf(buf, "Field2 time   %v\n", t.Field2)
-	//fmt.Fprintf(buf, "Field3 dict   %s\n", t.Field3)
-	//fmt.Fprintf(buf, "Field4 int    %d\n", t.Field4)
-	//fmt.Fprintf(buf, "Field5 float  %f\n", t.Field5)
-	//fmt.Fprintf(buf, "Field6 list   %s\n", t.Field6)
-	//fmt.Fprintf(buf, "Field7 string %s\n", t.Field7)
+	fmt.Fprintf(buf, "Id     String %s\n", t.Id)
+	fmt.Fprintf(buf, "Field1 Bool   %t\n", t.Field1)
+	//fmt.Fprintf(buf, "Field2 Time   %v\n", t.Field2)
+	fmt.Fprintf(buf, "Field3 Dict   %s\n", t.Field3)
+	fmt.Fprintf(buf, "Field4 Int    %d\n", t.Field4)
+	fmt.Fprintf(buf, "Field5 Float  %f\n", t.Field5)
+	//fmt.Fprintf(buf, "Field6 List   %s\n", t.Field6)
+	fmt.Fprintf(buf, "Field7 String %s\n", t.Field7)
+	fmt.Fprintf(buf, "Field8 String %s\n", t.Field8)
 	return buf.String()
 }
 
@@ -100,15 +106,6 @@ func main() {
 
 	SetupDB(db)
 
-	custom := Customs{}
-	CheckError(
-		db.Get(
-			&custom,
-			SelectSQL,
-			"test01",
-		))
-	fmt.Println(custom)
-
 	primative := Primatives{}
 	CheckError(
 		db.Get(
@@ -117,6 +114,15 @@ func main() {
 			"test01",
 		))
 	fmt.Println(primative)
+
+	custom := Customs{}
+	CheckError(
+		db.Get(
+			&custom,
+			SelectSQL,
+			"test01",
+		))
+	fmt.Println(custom)
 }
 
 func SetupDB(db *sqlx.DB) {
@@ -126,15 +132,19 @@ func SetupDB(db *sqlx.DB) {
 	tx.MustExec("DELETE FROM temporary")
 	for i := 0; i < 10; i++ {
 		tx.MustExec(
-			"INSERT INTO temporary (id, field1, field2, field3, field4, field5, field6, field7) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)",
+			`INSERT INTO temporary (id, field1, field2, field3, field4, field5, field6, field7, field8)
+			 VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)`,
 			fmt.Sprintf("test%02d", i),
 			true,
 			time.Now().UTC(),
-			"{}",
+			Dictionary{
+				"foo": "bar",
+			},
 			34,
 			4.5,
 			"[]",
 			"hello, world",
+			nil,
 		)
 	}
 	tx.Commit()
